f0e59892a9a3cded1477a5eaf629dc93ba119c26
juan <juanbindez780@gmail.com>
2024-08-06 21:15:55 -0300

    lemonpdf 1.0.0

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'fix cli'
-git push -u origin dev
-git tag v1.0-rc10
+git commit -m 'lemonpdf 1.0.0'
+git push -u origin main
+git tag v1.0.0
 git push --tag
 make clean
 make upload
