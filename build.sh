1e1facec136e42ceeef6118f9ba257377263d9a6
JuanBindez <juanbindez780@gmail.com>
2024-08-07 12:04:32 -0300

    lemonpdf 2.0.0

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'Add CLI options to extract URLs and domains from PDFs.
- 
-Implement -u option for URL extraction and -d option for domain extraction, with additional options for saving output to a file
-'
-git push -u origin dev
-git tag v2.0-rc2
+git commit -m 'lemonpdf 2.0.0'
+git push -u origin main
+git tag v2.0.0
 git push --tag
 make clean
 make upload
